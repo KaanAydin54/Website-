bb3edf11a525767e8216b5a13e0c7c35c60ff98e
KaanAydin54 <kaanaydin8754@gmail.com>
2024-12-09 19:32:16 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ben olmamak birşey</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Siteme Hoş Geldin!</h1>
+    <h2>Ben Küçük Başlık!</h2>
+    <h3>Bende Daha Küçük!</h3>
+
+    <a target = "_blank"href="https://www.youtube.com/watch?v=bI8P6ZSHSvE">
+    <button>Şarki için Bas</button></a>
+
+    </p>Herhangi birşey</p>
+    <h2>Hebele Hübele</h2>
+    <iframe src="https://www.meteoblue.com/tr/hava/widget/daily/%c4%b0stanbul_t%c3%bcrkiye-cumhuriyeti_745044?geoloc=fixed&days=7&tempunit=CELSIUS&windunit=KILOMETER_PER_HOUR&precipunit=MILLIMETER&coloured=coloured&pictoicon=0&maxtemperature=1&mintemperature=1&windspeed=1&windgust=1&winddirection=1&uv=0&humidity=0&precipitation=1&precipitationprobability=1&spot=1&pressure=0&layout=light"  frameborder="0" scrolling="NO" allowtransparency="true" sandbox="allow-same-origin allow-scripts allow-popups allow-popups-to-escape-sandbox" style="width: 378px; height: 420px"></iframe><div><!-- DO NOT REMOVE THIS LINK --><a href="https://www.meteoblue.com/tr/hava/hafta/%c4%b0stanbul_t%c3%bcrkiye-cumhuriyeti_745044?utm_source=daily_widget&utm_medium=linkus&utm_content=daily&utm_campaign=Weather%2BWidget" target="_blank" rel="noopener">meteoblue</a></div>
+     
+             <ul>
+                   <li>Bende hiçbirşey</li>
+                   <li>Bende üstteki ile ayn</li>
+             </ul>
+      <img width="1100" height="600" src="coding.jpg" alt="Image1">
+     
+</body>
+</html>
